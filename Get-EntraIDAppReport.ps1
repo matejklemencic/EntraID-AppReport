aa8e3ec1f74a7cffcfae531bd9fc51553e5b3a5d
Matej <matej@matej.guru>
2026-07-12 13:07:25 +0200

    Remove Microsoft Graph PowerShell from high-value target applications list

--- Get-EntraIDAppReport.ps1
+++ Get-EntraIDAppReport.ps1
@@ -137,7 +137,6 @@ $script:HighValueTargetApps = @{
     '1b730954-1685-4b74-9bfd-dac224a7b894' = 'Azure Active Directory PowerShell'
     'fb78d390-0c51-40cd-8e17-fdbfab77341b' = 'Exchange Online PowerShell'
     '14d82eec-204b-4c2f-b7e8-296a70dab67e' = 'Microsoft Graph Command Line Tools'
-    '09abbdfd-ed23-44ee-a2d9-a627aa1c90f3' = 'Microsoft Graph PowerShell'
 }
 
 # Risk scoring configuration
